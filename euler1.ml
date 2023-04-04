8c4eb91e7ed94790a7fc88a8121965c126d90c90
Jay Langford <24867831+jaylangford@users.noreply.github.com>
2023-04-04 13:50:54 -0600

    Add euler 1.

---
+++ euler1.ml
@@ -0,0 +1,14 @@
+let rec range i j = 
+  if i > j then [] 
+  else i :: range (i+1) j;;
+
+let rec euler_1 l =
+  match l with
+  | [] -> 0
+  | h :: t ->
+    match h mod 3 = 0 || h mod 5 = 0 with
+    | true -> h + euler_1 t
+    | false -> euler_1 t;;
+
+
+Printf.printf "%i\n" @@ euler_1 @@ range 0 999;;
